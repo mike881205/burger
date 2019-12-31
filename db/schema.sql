f8e97f95bfe6f4613f000a94d602936b90e01b13
mike881205 <mike881205@gmail.com>
2019-12-30 22:16:13 -0500

    Created DB schema and seeds, config files, controllers file, models file, public files, views files, and server file. Set up server connections & mysql connection.

---
+++ db/schema.sql
@@ -0,0 +1,17 @@
+### Schema
+
+DROP DATABASE IF EXISTS burgers_db;
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers
+(
+	id int NOT NULL AUTO_INCREMENT,
+	burger_name varchar(255) NOT NULL,
+	eaten BOOLEAN DEFAULT false,
+	PRIMARY KEY (id)
+);
+
+INSERT INTO burgers (burger_name) VALUES ('Royale w/ Cheese');
+INSERT INTO burgers (burger_name) VALUES ('Goodburger');
+INSERT INTO burgers (burger_name) VALUES ('Krusty Burger');
