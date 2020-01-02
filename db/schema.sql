ef4799cd33f9ff54c12412e4d6d8506fa8cee1c8
mike881205 <mike881205@gmail.com>
2020-01-02 18:29:20 -0500

    created Jaws DB

--- db/schema.sql
+++ db/schema.sql
@@ -11,7 +11,3 @@ CREATE TABLE burgers
 	eaten BOOLEAN DEFAULT false,
 	PRIMARY KEY (id)
 );
-
-INSERT INTO burgers (burger_name) VALUES ('Royale w/ Cheese');
-INSERT INTO burgers (burger_name) VALUES ('Goodburger');
-INSERT INTO burgers (burger_name) VALUES ('Krusty Burger');
